074ad6139aaa7d9ec7c9564cfcdd921d03ddfe0b
Jeroen van der Laan <jeroen.jeroenvdlaan@gmail.com>
2020-03-24 15:50:00 +0100

    home page tweaks

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -94,7 +94,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('welcome'));
+        return new RedirectResponse($this->urlGenerator->generate('home'));
 //        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
