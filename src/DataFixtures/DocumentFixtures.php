fac3528f83206ea4fc5c48f62423fa5ecbac3ff2
Jeroen van der Laan <jeroen.jeroenvdlaan@gmail.com>
2020-04-01 10:26:57 +0200

    DocumentFixtures random filename and discipline

--- src/DataFixtures/DocumentFixtures.php
+++ src/DataFixtures/DocumentFixtures.php
@@ -2,25 +2,29 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Building;
 use App\Entity\Discipline;
 use App\Entity\Document;
+use App\Faker\NewNativeLoader;
+use App\Faker\Provider\DocumentProvider;
 use App\Repository\BuildingRepository;
 use App\Repository\DisciplineRepository;
 use App\Service\DocumentNameParserService;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
-use Doctrine\Common\Persistence\ObjectManager;
 use Nelmio\Alice\Loader\NativeLoader;
 
 
 class DocumentFixtures extends Fixture implements DependentFixtureInterface
 {
     private $disciplines;
+    private $buildings;
+    private $nameParser;
 
-    public function __construct(DisciplineRepository $dr)
+    public function __construct(DisciplineRepository $dr, BuildingRepository $br)
     {
         $this->disciplines = $dr->findAll();
+        $this->buildings = $br->findAll();
+        $this->nameParser = new DocumentNameParserService();
     }
 
     /**
@@ -29,25 +33,44 @@ class DocumentFixtures extends Fixture implements DependentFixtureInterface
     public function load(\Doctrine\Persistence\ObjectManager $manager)
     {
         $loader = new NativeLoader();
-        $objectset = $loader->loadData([\App\Entity\Document::class => [
-            'document{1..10}' => [
-                'discipline' => $this->getRandomDiscipline(),
-                'file_name' => "testfile.pdf",
-                'updated_at' => '<dateTime("now")>',
-                'description' => '<text()>'
+        /** @var Document[] $objectset */
+        $objectset = $loader->loadData(
+            [\App\Entity\Document::class =>
+                [
+                    'document{1..10}' => [
+                        'updated_at' => '<dateTime("now")>',
+                        'description' => '<text()>'
+                    ]
+                ]
             ]
-        ]])->getObjects();
+        )->getObjects();
 
         foreach ($objectset as $object)
         {
+            $randomDiscipline = $this->getRandomDiscipline();
+
+            $object->setFileName($this->generateRandomFilename($randomDiscipline));
+
+            $object->setDiscipline($this->getRandomDiscipline());
+
             $manager->persist($object);
         }
         $manager->flush();
     }
 
-    public function getRandomDiscipline() : Discipline {
+    public function getRandomDiscipline() : Discipline
+    {
         $key = array_rand($this->disciplines);
         return $this->disciplines[$key];
+    }
+
+    public function generateRandomFilename(Discipline $discipline): string
+    {
+        $key = array_rand($this->buildings);
+
+        $randomBuilding = $this->buildings[$key];
+
+        return $this->nameParser->generateFileNameFromEntities($randomBuilding, $discipline, rand(0, 4), 1);
     }
 
 
