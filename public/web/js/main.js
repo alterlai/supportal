d8e51c61141fe000b5da7dbc3508a37792391ab7
Jeroen van der Laan <jeroen.jeroenvdlaan@gmail.com>
2020-05-11 16:51:17 +0200

    Melding weergegeven wanneer geen documenten zijn gevonden bij zoeken

--- public/web/js/main.js
+++ public/web/js/main.js
@@ -109,34 +109,36 @@ $(document).ready(function() {
      * Populate documents page with documents
      */
     function updateDocumentCards($documents) {
-        // Clear tileContainer of existing documents
-        $('.tileContainer').html('');
-        console.log($documents);
-
-        $documents.forEach(function($doc, index)
-        {
-            console.log($doc)
-            $('.tileContainer').append(
-                "<div class=\'documentCard\'>\n" +
-                "                        <div class=\"pdfPreview\">\n" +
-                "                            <svg class=\"bi bi-image-fill\" width=\"1em\" height=\"1em\" viewBox=\"0 0 16 16\" fill=\"currentColor\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
-                "                                <path fill-rule=\"evenodd\" d=\"M.002 3a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2h-12a2 2 0 01-2-2V3zm1 9l2.646-2.354a.5.5 0 01.63-.062l2.66 1.773 3.71-3.71a.5.5 0 01.577-.094L15.002 9.5V13a1 1 0 01-1 1h-12a1 1 0 01-1-1v-1zm5-6.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z\" clip-rule=\"evenodd\"/>\n" +
-                "                            </svg>\n" +
-                "                        </div>\n" +
-                "                        <div class=\"documentDetails\">\n" +
-                "                            <ul>\n" +
-                "                                <li>"+ $doc.naam + "</li>\n" +
-                "                                <li>"+ $doc.locatie +"</li>\n" +
-                "                                <li>"+  ($doc.gebouw ? $doc.gebouw : "") +"</li>\n" +
-                "                                <li>"+  ($doc.area ? $doc.area : "") + "</li>\n" +
-                "                                <li>Verdieping: " +   $doc.verdieping + "</li>\n" +
-                "                                <li>"+  $doc.discipline + $doc.disciplineOmschrijving + "</li>\n" +
-                "                                <li>"+  $doc.updatedAt + "</li>\n" +
-                "                                <li>" + $doc.documentSoort + "</li>\n" +
-                "                            </ul>\n" +
-                "                        </div>\n" +
-                "                    </div>");
-        })
+        if ($documents.length > 0) {
+            // Clear tileContainer of existing documents
+            $('.tileContainer').html('');
+            $documents.forEach(function ($doc, index) {
+                console.log($doc)
+                $('.tileContainer').append(
+                    "<div class=\'documentCard\'>\n" +
+                    "                        <div class=\"pdfPreview\">\n" +
+                    "                            <svg class=\"bi bi-image-fill\" width=\"1em\" height=\"1em\" viewBox=\"0 0 16 16\" fill=\"currentColor\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+                    "                                <path fill-rule=\"evenodd\" d=\"M.002 3a2 2 0 012-2h12a2 2 0 012 2v10a2 2 0 01-2 2h-12a2 2 0 01-2-2V3zm1 9l2.646-2.354a.5.5 0 01.63-.062l2.66 1.773 3.71-3.71a.5.5 0 01.577-.094L15.002 9.5V13a1 1 0 01-1 1h-12a1 1 0 01-1-1v-1zm5-6.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z\" clip-rule=\"evenodd\"/>\n" +
+                    "                            </svg>\n" +
+                    "                        </div>\n" +
+                    "                        <div class=\"documentDetails\">\n" +
+                    "                            <ul>\n" +
+                    "                                <li>" + $doc.naam + "</li>\n" +
+                    "                                <li>" + $doc.locatie + "</li>\n" +
+                    "                                <li>" + ($doc.gebouw ? $doc.gebouw : "") + "</li>\n" +
+                    "                                <li>" + ($doc.area ? $doc.area : "") + "</li>\n" +
+                    "                                <li>Verdieping: " + $doc.verdieping + "</li>\n" +
+                    "                                <li>" + $doc.discipline + $doc.disciplineOmschrijving + "</li>\n" +
+                    "                                <li>" + $doc.updatedAt + "</li>\n" +
+                    "                                <li>" + $doc.documentSoort + "</li>\n" +
+                    "                            </ul>\n" +
+                    "                        </div>\n" +
+                    "                    </div>");
+            })
+        }
+        else {
+            $('.tileContainer').html("<h2>Geen documenten gevonden met deze criteria.</h2>");
+        }
     }
 });
 
